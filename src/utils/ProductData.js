434093aac957678492bd96ea9f8479788e9ebc0e
jihanapriliani <11211046@student.itk.ac.id>
2023-06-07 02:27:31 +0800

    fixing desc order

--- src/utils/ProductData.js
+++ src/utils/ProductData.js
@@ -1,28 +1,31 @@
-  class QuickSort {
-    static sort(arr, option, orderBy) {
-      if (arr.length <= 1) {
-        return arr;
+class QuickSort {
+  static sort(arr, option, orderBy) {
+    if (arr.length <= 1) {
+      return arr;
+    }
+
+    const pivot = arr[arr.length - 1];
+    const left = [];
+    const right = [];
+
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i][option] < pivot[option]) {
+        left.push(arr[i]);
+      } else {
+        right.push(arr[i]);
       }
+    }
 
-      const pivot = arr[arr.length - 1];
-      const left = [];
-      const right = [];
-
-      for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i][option] < pivot[option]) {
-          left.push(arr[i]);
-        } else {
-          right.push(arr[i]);
-        }
-      }
-
-      if(orderBy == "desc") {
-        return [...QuickSort.sort(right, option), pivot, ...QuickSort.sort(left, option)];
-      } 
-      return [...QuickSort.sort(left, option), pivot, ...QuickSort.sort(right, option)];
-
+    if (orderBy === "desc") {
+      return [...QuickSort.sort(right, option, orderBy), pivot, ...QuickSort.sort(left, option, orderBy)];
+    } else if (orderBy === "asc") {
+      return [...QuickSort.sort(left, option, orderBy), pivot, ...QuickSort.sort(right, option, orderBy)];
+    } else {
+      throw new Error("Invalid orderBy option. Must be 'asc' or 'desc'.");
     }
   }
+}
+
 
   class Node {
     constructor(id, nama_produk, supplier, harga_po, harga_jual) {
@@ -120,26 +123,27 @@
   export default function sortTree(option, orderBy) {
     const bst = new BinarySearchTree(option, orderBy);
 
-    bst.insert(1, "Produk A", "Supplier A", 200, 300);
-    bst.insert(2, "Produk B", "Supplier B", 150, 250);
-    bst.insert(3, "Produk C", "Supplier C", 180, 280);
-    bst.insert(4, "Produk D", "Supplier D", 220, 350);
-    bst.insert(5, "Produk E", "Supplier A", 190, 320);
-    bst.insert(6, "Produk F", "Supplier B", 160, 270);
-    bst.insert(7, "Produk G", "Supplier C", 210, 310);
-    bst.insert(8, "Produk H", "Supplier D", 240, 380);
-    bst.insert(9, "Produk I", "Supplier A", 205, 290);
-    bst.insert(10, "Produk J", "Supplier B", 170, 260);
-    bst.insert(11, "Produk K", "Supplier C", 195, 330);
-    bst.insert(12, "Produk L", "Supplier D", 230, 360);
-    bst.insert(13, "Produk M", "Supplier A", 215, 295);
-    bst.insert(14, "Produk N", "Supplier B", 185, 245);
-    bst.insert(15, "Produk O", "Supplier C", 225, 315);
-    bst.insert(16, "Produk P", "Supplier D", 260, 370);
-    bst.insert(17, "Produk Q", "Supplier A", 250, 310);
-    bst.insert(18, "Produk R", "Supplier B", 220, 290);
-    bst.insert(19, "Produk S", "Supplier C", 270, 350);
-    bst.insert(20, "Produk T", "Supplier D", 290, 400);
+    bst.insert(1, "Pensil", "CV. Alat Tulis Indonesia", 2000, 3000);
+    bst.insert(2, "Penghapus", "PT. Stationery Utama", 1500, 2000);
+    bst.insert(3, "Pensil Warna", "CV. Pelangi Kreatif", 5000, 7000);
+    bst.insert(4, "Penggaris", "PT. Alat Ukur Presisi", 3000, 4000);
+    bst.insert(5, "Buku Catatan", "CV. Bina Grafika", 4000, 6000);
+    bst.insert(6, "Stapler", "PT. Mesin Jepit Prima", 8000, 10000);
+    bst.insert(7, "Amplop", "CV. Surat Menyurat Sejahtera", 3000, 4000);
+    bst.insert(8, "Spidol", "PT. Warna Ceria", 5000, 7000);
+    bst.insert(9, "Kuas Cat", "CV. Seni Rupa Makmur", 6000, 8000);
+    bst.insert(10, "Pisau Cutter", "PT. Alat Potong Terampil", 4000, 5500);
+    bst.insert(11, "Buku Agenda", "CV. Agendaku", 6000, 8000);
+    bst.insert(12, "Tipe-X", "PT. Kertas Ketik Kilat", 2000, 3000);
+    bst.insert(13, "Pita Perekat", "CV. Lembut Rapi", 2500, 3500);
+    bst.insert(14, "Kertas HVS", "PT. Kertas Putih Bersih", 7000, 9000);
+    bst.insert(15, "Kalkulator", "CV. Matematika Maju", 10000, 15000);
+    bst.insert(16, "Kertas Foto", "PT. Fotografi Berkualitas", 8000, 10000);
+    bst.insert(17, "Papan Tulis", "CV. Kreatif Kreasi", 15000, 20000);
+    bst.insert(18, "Pensil Mekanik", "PT. Alat Tulis Canggih", 5000, 7000);
+    bst.insert(19, "Tempat Pensil", "CV. Rapi dan Tertata", 3000, 4000);
+    bst.insert(20, "Kuas Lukis", "PT. Seni Visual Nusantara", 7000, 9000);
+    
 
     return bst.getSortedArray();
   }
